33313f705dc5aeb71799ff6fbf5225ffb1f928c1
LanternControl <brett4president@hotmail.com>
2017-08-17 15:03:51 -0400

    Server up and running with required routes

---
+++ app.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let expressValidator = require('express-validator');
+let mustache = require('mustache');
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'mustache');
+
+app.get('/', function (req, res) {
+   res.send('hello');
+});
+
+
+
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+});
