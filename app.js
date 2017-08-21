0124c9c6b858dfd1179e6851d8ca04aa5446f60a
LanternControl <brett4president@hotmail.com>
2017-08-20 21:19:14 -0400

    got a list working now, looks wonky

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-let expressValidator = require('express-validator');
+// let expressValidator = require('express-validator');
 let mustache = require('mustache-express');
-let data = require('./data.js');
 let app = express();
+
+let list = [];
+let finished = [];
 
 app.engine('mustache', mustache());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator());
+// app.use(expressValidator());
 
-app.set('views', '/views');
+app.set('views', './views');
 app.set('view engine', 'mustache');
 
-app.get('/', function (req, res) {
-   res.render('index', data);
+app.get('/', function (req, res){
+  res.render('index', { list: list, finished: finished});
 });
 
 app.post('/', function (req, res){
-  data.push(req.body.data);
+  list.push(req.body.todoText);
+  res.redirect('/');
+});
+
+app.post('/add', function (req, res){
+  finished.push(req.body.done);
   res.redirect('/');
 });
 
